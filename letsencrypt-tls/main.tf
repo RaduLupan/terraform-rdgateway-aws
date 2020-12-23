491fd2603e92f3503328cb371d658e16d038cb01
RaduLupan <github@lupan.ca>
2020-12-23 09:17:22 -0500

    Added data source for route 53 zone and corresponding local for zone_id, added IAM policy for lambda based on template and attached to lambda role

--- letsencrypt-tls/main.tf
+++ letsencrypt-tls/main.tf
@@ -3,8 +3,10 @@ provider "aws" {
 }
 
 locals {
-  s3_name = "${var.s3_prefix}-${var.environment}-${lower(random_string.random.result)}"
+  s3_name        = "${var.s3_prefix}-${var.environment}-${lower(random_string.random.result)}"
   s3_bucket_name = split(".", aws_s3_bucket.certbot.bucket_domain_name)[0]
+
+  route53_zone_id = data.aws_route53_zone.selected.zone_id
 
   common_tags = {
     terraform   = "true"
@@ -12,6 +14,13 @@ locals {
     role        = "letsencrypt-tls"
   }
 }
+
+# Use this data source to get the Route 53 zone ID.
+data "aws_route53_zone" "selected" {
+  name         = var.route53_public_zone
+  private_zone = false
+}
+
 
 # The random string needed for injecting randomness in the name of the S3 bucket.
 resource "random_string" "random" {
@@ -76,4 +85,25 @@ data "aws_iam_policy_document" "assume_execution" {
       identifiers = ["lambda.amazonaws.com"]
     }
   }
+}
+
+# Template for lambda certbot IAM policy.
+data "template_file" "le-certbot-lambda-policy" {
+  template = file("${path.module}/le-certbot-lambda-policy.json.tpl")
+
+  vars = {
+    s3_bucket_arn    = aws_s3_bucket.certbot.arn
+    route53_zone_arn = "arn:aws:route53:::hostedzone/${local.route53_zone_id}"
+  }
+}
+
+# Lambda certbot IAM policy based on template.
+resource "aws_iam_policy" "le-certbot-lambda-policy" {
+   policy = data.template_file.le-certbot-lambda-policy.rendered
+}
+
+# Lambda policy attachment to Lambda execution role.
+resource "aws_iam_role_policy_attachment" "le-certbot-lambda-policy-attach" {
+  role       = aws_iam_role.execution.name
+  policy_arn = aws_iam_policy.le-certbot-lambda-policy.arn
 }

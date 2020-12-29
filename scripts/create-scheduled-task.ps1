78f9521cd4c99472db4fc1833ad41332fb0394fd
RaduLupan <github@lupan.ca>
2020-12-29 14:59:11 -0500

    Introduced switch based on the script that is scheduled so that the renew script is run daily and the get-latest script is run at system startup

--- scripts/create-scheduled-task.ps1
+++ scripts/create-scheduled-task.ps1
@@ -1,15 +1,29 @@
 ﻿param (
-    [string[]] $psScript='C:\scripts\renew-letsencrypt-tls.ps1',
+    [string] $psScript,
     [string] $Region,
     [string] $S3Bucket,
+    [string] $S3Folder,
     [string] $SQSUrl
 )
 
-
 $TaskName=$psScript.Split('\')[-1]
 
-$stAction = New-ScheduledTaskAction -Execute “powershell.exe” -Argument “-Command $psScript -Region $Region -S3Bucket $S3Bucket -SQSUrl $SQSUrl -Install”
-$stTrigger =  New-ScheduledTaskTrigger -Daily -At 3am
+switch ($psScript) {
+    
+    # Schedule renew-letsencrypt-tls.ps1 script to run daily to catch the renewal date.
+    'C:\scripts\renew-letsencrypt-tls.ps1' {
+        $stAction = New-ScheduledTaskAction -Execute “powershell.exe” -Argument “-Command $psScript -Region $Region -S3Bucket $S3Bucket -SQSUrl $SQSUrl -Install”
+        $stTrigger =  New-ScheduledTaskTrigger -Daily -At 3am            
+    }
+    
+    # Schedule get-latest-letsencrypt-tls.ps1 to run at startup in case the instance missed the renewal.
+    'C:\scripts\get-latest-letsencrypt-tls.ps1' {
+        $stAction = New-ScheduledTaskAction -Execute “powershell.exe” -Argument “-Command $psScript -Region $Region -S3Bucket $S3Bucket -S3Folder $S3Folder"
+        $stTrigger =  New-ScheduledTaskTrigger -AtStartup
+    }
+}
+
+
 $stPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType S4U -RunLevel Highest
 $stSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 01:00:00 -Compatibility Win8
     

c328a44c458d244aee51ceca7ece17bdcc9bc4f0
RaduLupan <github@lupan.ca>
2020-12-29 09:15:41 -0500

    Refactored script to download the latest certificate from S3 bucket.

--- scripts/get-latest-letsencrypt-tls.ps1
+++ scripts/get-latest-letsencrypt-tls.ps1
@@ -1,48 +1,39 @@
-﻿param (
+﻿<# 
+If the RD Gateway is on only during business hours schedule this script to run at startup to ensure that the latest certificate
+deposited in the S3 bucket by the certbot Lambda is downloaded and installed on the RD Gateway server.
+#>
 
-    [string] $ParamsFile='C:\latest\RDPGW-Params.csv',
+param (
     [string] $LocalPath='C:\tls',
-    [string] $PFXPassword=-join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_})
+    [string] $PFXPassword=-join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_}),
+    [string] $Region,
+    [string] $S3Bucket,
+    [string] $S3Subfolder
 )
 
+# Download the public and private keys from S3 bucket folder letsencrypt-tls where the certbot Lambda saved them.
+$CertPrivateKey="letsencrypt-tls/$S3Subfolder/privkey.pem"
+$CertPublicKey="letsencrypt-tls/$S3Subfolder/fullchain.pem"
 
-$Params=Import-Csv $ParamsFile
-
-## Get instance public IP from instance meta-data
-$PublicIP=(Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/public-ipv4 -UseBasicParsing).Content
-
-## Select the RDPGW based on the public IP
-$RDPGW=$Params | Where-Object {$_.IP -eq $PublicIP}
-
-$Region=$RDPGW.SQSQueueURL.Split('.')[1]
-
-## Download the public and private keys from S3
-
-$CertPrivateKey="letsencrypt-ssl/$($RDPGW.HostName -replace "rdpgw.`",`"`")/privkey.pem"
-$CertPublicKey="letsencrypt-ssl/$($RDPGW.HostName -replace "rdpgw.`",`"`")/fullchain.pem"
-
-Read-S3Object -BucketName $RDPGW.S3Bucket -Key $CertPrivateKey -File "$LocalPath\cert-private.pem" -Region $Region
-
-Read-S3Object -BucketName $RDPGW.S3Bucket -Key $CertPublicKey -File "$LocalPath\cert-public.pem" -Region $Region
+Read-S3Object -BucketName $S3Bucket -Key $CertPrivateKey -File "$LocalPath\cert-private.pem" -Region $Region
+Read-S3Object -BucketName $S3Bucket -Key $CertPublicKey -File "$LocalPath\cert-public.pem" -Region $Region
 
 $CurrentFolder=Get-Location
 
 Set-Location $LocalPath
 
+# Use OpenSSL to create .pfx file from the public certificate and private key PEM format.
 openssl pkcs12 -export -out certificate.pfx -inkey cert-private.pem -in cert-public.pem -passout pass:$PFXPassword
 
+# Import the certificate from .pfx file, add it to the RD Gateway server and restart tsgateway service.
 Import-Module RemoteDesktopServices
-
 $Certificate=Get-PfxData -FilePath certificate.pfx  -Password (ConvertTo-SecureString -String $PFXPassword -Force -AsPlainText) | Select-Object -ExpandProperty EndEntityCertificates
-
 Import-PfxCertificate -FilePath certificate.pfx -Password (ConvertTo-SecureString -String $PFXPassword -Force -AsPlainText) -Exportable -CertStoreLocation Cert:\LocalMachine\My
-
 $MostRecentCert=(Get-ChildItem cert:\localmachine\my | where-object {$_.Subject -eq $Certificate.Subject} | Sort-Object -Property NotAfter -Descending | Select-Object -First 1)
-
 Set-Item -Path RDS:\GatewayServer\SSLCertificate\Thumbprint -Value $MostRecentCert.Thumbprint
-
 Restart-Service tsgateway
 
+# Cleanup $LocalPath folder.
 Remove-Item certificate.pfx,cert-private.pem,cert-public.pem -Force
 
 Set-Location $CurrentFolder

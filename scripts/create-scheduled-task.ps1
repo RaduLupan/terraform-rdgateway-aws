d7f6eae52a991f031d803fc42527c4bbcf2fe4c7
RaduLupan <github@lupan.ca>
2020-12-29 07:43:54 -0500

    Added parameters , ,  and injected them in the create-scheduled-task.ps1 invokation.

--- scripts/create-scheduled-task.ps1
+++ scripts/create-scheduled-task.ps1
@@ -1,12 +1,14 @@
 ﻿param (
-
-    [string[]] $psScript='C:\scripts\renew-letsencrypt-tls.ps1'
+    [string[]] $psScript='C:\scripts\renew-letsencrypt-tls.ps1',
+    [string] $Region,
+    [string] $S3Bucket,
+    [string] $SQSUrl
 )
 
 
 $TaskName=$psScript.Split('\')[-1]
 
-$stAction = New-ScheduledTaskAction -Execute “powershell.exe” -Argument “-Command $psScript  -Install”
+$stAction = New-ScheduledTaskAction -Execute “powershell.exe” -Argument “-Command $psScript -Region $Region -S3Bucket $S3Bucket -SQSUrl $SQSUrl -Install”
 $stTrigger =  New-ScheduledTaskTrigger -Daily -At 3am
 $stPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType S4U -RunLevel Highest
 $stSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 01:00:00 -Compatibility Win8
@@ -14,7 +16,3 @@ $stSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit 01:00:00 -Compati
 Register-ScheduledTask -TaskName $TaskName -Action $stAction -Trigger $stTrigger -Principal $stPrincipal -Settings $stSettings
 
 Write-Output "`n Task $TaskName created!"
-
-
-
-

2316236332e403563e0979c8a2d7eb05529f0af2
RaduLupan <github@lupan.ca>
2020-12-30 08:28:44 -0500

    Corrected the variable interpolation

--- rd-gateway/user-data.ps1
+++ rd-gateway/user-data.ps1
@@ -57,5 +57,5 @@ $GroupName='Administrators';$DomainNetBiosName='BUILTIN'
 New-Item -path RDS:\GatewayServer\CAP -Name Default-CAP -UserGroups "$GroupName@$DomainNetBiosName" -AuthMethod 1
 New-Item -Path RDS:\GatewayServer\RAP -Name Default-RAP -UserGroups "$GroupName@$DomainNetBiosName" -ComputerGroupType 2
 Restart-Service tsgateway
-Rename-Computer -NewName {computer_name} -Force
+Rename-Computer -NewName "${computer_name}" -Force
 </powershell>

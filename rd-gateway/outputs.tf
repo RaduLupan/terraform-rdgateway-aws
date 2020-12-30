d521f9a5b3afaad09abcb78ca574b9474babbf22
RaduLupan <github@lupan.ca>
2020-12-30 14:29:54 -0500

    Added rdgw security group as output

--- rd-gateway/outputs.tf
+++ rd-gateway/outputs.tf
@@ -2,3 +2,8 @@ output "rdgw_eip" {
   description = "The public IP associated with the RD Gateway instance"
   value       = aws_eip.main.public_ip
 }
+
+output "rdgw_sg" {
+  description = "The ID of the security group associated with the RD Gateway"
+  value       = aws_security_group.main.id
+}

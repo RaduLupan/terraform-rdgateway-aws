00c0dfc06cc7d3f3f7dcfe5346e2b1fe8f9f4808
RaduLupan <github@lupan.ca>
2021-01-28 16:13:48 -0500

    Download ODT installer package, extract installer and config files, install RDS Windows features to convert the server into RDS Host and Licensing server

--- rds-host/user-data.ps1
+++ rds-host/user-data.ps1
@@ -1,7 +1,24 @@
 <powershell>
-$ComputerName="${computer_name}"
+$computerName="${computer_name}"
+$url="${download_url}"
 
 Set-TimeZone -Name "Eastern Standard Time"
+
+if (! (Test-Path "C:\ODT")) {
+    New-Item -Path "C:\ODT" -ItemType Container | Out-Null
+}
+
+if (! (Test-Path "C:\ODT\Logs")) {
+    New-Item -Path "C:\ODT\Logs" -ItemType Container | Out-Null
+}
+
+$output = "C:\ODT\odt-installer.exe"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+
+Invoke-Expression "$output /extract:C:\ODT /quiet"
+
+Install-WindowsFeature RDS-RD-Server, RDS-Licensing, RSAT-RDS-Tools -IncludeManagementTools
 
 Rename-Computer -NewName $ComputerName -Force
 </powershell>

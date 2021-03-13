e2b30cfd0d97c2c62addff367b6675cf28c88c8e
RaduLupan <github@lupan.ca>
2021-03-13 07:47:48 -0500

    Added the configuration XML file for Office Deployment Tool as in-line here-string

--- rds-host/user-data.ps1
+++ rds-host/user-data.ps1
@@ -2,15 +2,38 @@
 $computerName="${computer_name}"
 $url="${download_url}"
 
+$configurationXML=@"
+<Configuration>
+<Add SourcePath="\\localhost\Software\" OfficeClientEdition="32" >
+<Product ID="O365ProPlusRetail">
+<Language ID="en-us" />
+<ExcludeApp ID="Teams" />
+</Product>
+</Add>
+
+<!-- <Updates Enabled="TRUE" UpdatePath="\\Server\Share\" /> -->
+
+<Display Level="None" AcceptEULA="TRUE" />
+<Property Name="SharedComputerLicensing" Value="1" />
+
+<!-- <Logging Path="%temp%" /> -->
+
+<!-- <Property Name="AUTOACTIVATE" Value="1" /> -->
+
+</Configuration>
+"@
+
 Set-TimeZone -Name "Eastern Standard Time"
 
 if (! (Test-Path "C:\ODT")) {
     New-Item -Path "C:\ODT" -ItemType Container | Out-Null
 }
 
-if (! (Test-Path "C:\ODT\Logs")) {
-    New-Item -Path "C:\ODT\Logs" -ItemType Container | Out-Null
+if (! (Test-Path "C:\Software")) {
+    New-Item -Path "C:\Software" -ItemType Container | Out-Null
 }
+
+New-SmbShare -Name Software -Path "C:\Software" -FullAccess "Authenticated Users"
 
 $output = "C:\ODT\odt-installer.exe"
 
@@ -18,7 +41,12 @@ Invoke-WebRequest -Uri $url -OutFile $output
 
 Invoke-Expression "$output /extract:C:\ODT /quiet"
 
+$configurationXML | Set-Content -Path C:\ODT\configuration.xml -Encoding Ascii
+
 Install-WindowsFeature RDS-RD-Server, RDS-Licensing, RSAT-RDS-Tools -IncludeManagementTools
+
+"C:\ODT\setup.exe /download configuration.xml" | Set-Content -Path "C:\ODT\Office365-Download.cmd"
+"C:\ODT\setup.exe /configure configuration.xml" | Set-Content -Path "C:\ODT\Office365-Install.cmd"
 
 Rename-Computer -NewName $ComputerName -Force
 </powershell>
